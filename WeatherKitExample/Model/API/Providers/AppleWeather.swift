87ebfa56c7a62b9711ff0375bcb6609b469f6795
Masoud Sh <zzmasoud@gmail.com>
2022-10-22 10:29:55 +0200

    add mapping method to map from `DayWeather` to custom `Weather` model

--- WeatherKitExample/Model/API/Providers/AppleWeather.swift
+++ WeatherKitExample/Model/API/Providers/AppleWeather.swift
@@ -24,6 +24,9 @@ class AppleWeather: WeatherService {
         
         do {
             let result = try await service.weather(for: geo)
+            let weathers = result.dailyForecast.map({$0.mapToWeather()})
+            return Just(weathers).eraseToAnyPublisher()
+            
         } catch {
             return Empty().eraseToAnyPublisher()
         }
@@ -36,5 +39,17 @@ extension AppleWeather {
             return nil
         }
         return CLLocation(latitude: latitude, longitude: longitude)
+    }
+}
+
+extension DayWeather {
+    func mapToWeather() -> Weather {
+        return Weather(
+            temprature: highTemperature,
+            maxTemprature: highTemperature,
+            minTemprature: lowTemperature,
+            humidity: -1,
+            label: condition.description,
+            date: date)
     }
 }
