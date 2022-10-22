291768e49600d0f521615adfa72be01f4aad3b17
Masoud Sh <zzmasoud@gmail.com>
2022-10-22 09:30:28 +0200

    Initial Commit

---
+++ WeatherKitExample/WeatherKitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherKitExampleApp.swift
+//  WeatherKitExample
+//
+//  Created by Masoud Sheikh Hosseini on 10/22/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherKitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
