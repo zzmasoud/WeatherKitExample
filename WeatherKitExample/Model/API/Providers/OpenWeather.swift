6a8f5097bd851a16ccbdfdaf6ad73e2f2fa7c74e
Masoud Sh <zzmasoud@gmail.com>
2022-10-23 18:25:20 +0200

    Add a custom mapper to map API JSON to `Weather` model

--- WeatherKitExample/Model/API/Providers/OpenWeather.swift
+++ WeatherKitExample/Model/API/Providers/OpenWeather.swift
@@ -12,6 +12,19 @@ class OpenWeather: WeatherService {
     static let apiKey = "05b620cf268c91d00a8a7291d39b2ecb"
     
     func forecast(forLocation: Location) -> AnyPublisher<[Weather], Never> {
+        URLSession.shared.dataTaskPublisher(for: Self.apiUrl)
+            .map(\.data)
+            .map(OpenWeatherMapper.map(_:))
+            .eraseToAnyPublisher()
+        
         return Empty().eraseToAnyPublisher()
+    }
+}
+
+private class OpenWeatherMapper {
+    private init() {}
+    
+    static func map(_ data: Data) -> [Weather] {
+        return []
     }
 }
