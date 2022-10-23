5b6a401a8a1eb2d0eb1161984971b3a3bdb15f05
Masoud Sh <zzmasoud@gmail.com>
2022-10-23 18:41:15 +0200

    Add base api url

--- WeatherKitExample/Model/API/Providers/OpenWeather.swift
+++ WeatherKitExample/Model/API/Providers/OpenWeather.swift
@@ -10,6 +10,7 @@ import Combine
 
 class OpenWeather: WeatherService {
     static let apiKey = "05b620cf268c91d00a8a7291d39b2ecb"
+    static let apiUrl = URL(string: "https://api.openweathermap.org/data/3.0/onecall?")!
     
     func forecast(forLocation: Location) -> AnyPublisher<[Weather], Never> {
         URLSession.shared.dataTaskPublisher(for: Self.apiUrl)
@@ -25,6 +26,27 @@ private class OpenWeatherMapper {
     private init() {}
     
     static func map(_ data: Data) -> [Weather] {
-        return []
+        guard let root = try? JSONDecoder().decode(Root.self, from: data)
+        else { return [] }
+        
+        let weathers = root.daily
+        return weathers.map({map($0)})
+    }
+    
+    private static func map(_ weather: _Weather) -> Weather {
+        return Weather(
+            temprature: weather.temp.day.toMeasurement,
+            maxTemprature: weather.temp.max.toMeasurement,
+            minTemprature: weather.temp.min.toMeasurement,
+            humidity: weather.humidity,
+            label: weather.overall.main,
+            date: Date(timeIntervalSince1970: weather.dt)
+        )
+    }
+}
+
+fileprivate extension Double {
+    var toMeasurement: Measurement<UnitTemperature> {
+        return Measurement<UnitTemperature>(value: self, unit: .celsius)
     }
 }
