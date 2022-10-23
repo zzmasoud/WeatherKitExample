15fcf78b5e2bde8f427a58f6876887025fb040bc
Masoud Sh <zzmasoud@gmail.com>
2022-10-23 17:53:38 +0200

    Test forecast by cityName publishes empty array

--- WeatherKitExampleTests/WeatherKitExampleTests.swift
+++ WeatherKitExampleTests/WeatherKitExampleTests.swift
@@ -6,30 +6,30 @@
 //
 
 import XCTest
+import Combine
 
 final class WeatherKitExampleTests: XCTestCase {
+    
+    var subscriptions = [AnyCancellable]()
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_forecastByCityName_deliversEmpty() {
+        let sut = AppleWeather()
+        let cityName = "Berlin"
+        
+        let exp = expectation(description: "waiting...")
+        sut.forecast(forLocation: .city(name: cityName))
+            .sink(receiveValue: { forecasts in
+                XCTAssertEqual(forecasts, [])
+                exp.fulfill()
+            })
+            .store(in: &subscriptions)
+        
+        wait(for: [exp], timeout: 1)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    override func tearDown() {
+        super.tearDown()
+        subscriptions = []
     }
 
 }
