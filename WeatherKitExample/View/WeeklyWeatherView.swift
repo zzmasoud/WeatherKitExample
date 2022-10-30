2e5a0268e435c6385ceb04a6a0d6aee13719041c
Masoud Sh <zzmasoud@gmail.com>
2022-10-30 13:12:39 +0100

    Improve UI components by adding Navigation and List

--- WeatherKitExample/View/WeeklyWeatherView.swift
+++ WeatherKitExample/View/WeeklyWeatherView.swift
@@ -9,25 +9,44 @@ import SwiftUI
 
 struct WeeklyWeatherView: View {
     @ObservedObject var viewModel: WeeklyWeatherVM
-
+    
     init(viewModel: WeeklyWeatherVM) {
-      self.viewModel = viewModel
+        self.viewModel = viewModel
     }
-
+    
     var body: some View {
-        searchField
-        Text("RESULTS:")
-        Text(viewModel.city)
-        Section {
-            ForEach(viewModel.forecasts, content: { _ in DailyWeatherRow() })
+        NavigationView {
+            List {
+                searchField
+                
+                if viewModel.forecasts.isEmpty {
+                    emptySection
+                } else {
+                    forecastsView
+                }
+            }
+            .listStyle(GroupedListStyle())
+            .navigationBarTitle("ZZWeatherKit")
         }
     }
 }
 
 private extension WeeklyWeatherView {
     var searchField: some View {
-      HStack(alignment: .center) {
-        TextField("e.g. London", text: $viewModel.city)
-      }
+        HStack(alignment: .center) {
+            TextField("e.g. London", text: $viewModel.city)
+        }
     }
+    
+    var emptySection: some View {
+        Text("Start typing a city name ...")
+            .foregroundColor(.secondary)
+    }
+    
+    var forecastsView: some View {
+        Section {
+            ForEach(viewModel.forecasts, content: { _ in DailyWeatherRow() })
+        }
+    }
+    
 }
