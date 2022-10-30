a4f87e850a8805d325d62f9aaa99e6081e1efc64
Masoud Sh <zzmasoud@gmail.com>
2022-10-30 13:17:29 +0100

    Define `DailyWeatherRowVM` and its properties

--- WeatherKitExample/Model/ViewModel/DailyWeatherRowVM.swift
+++ WeatherKitExample/Model/ViewModel/DailyWeatherRowVM.swift
@@ -8,5 +8,37 @@
 import Foundation
 
 class DailyWeatherRowVM {
-    init() {}
+    private let model: Weather
+    
+    private static let dayFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "DD"
+        return formatter
+    }()
+    
+    private static let monthFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM"
+        return formatter
+    }()
+    
+    init(weather: Weather) {
+        self.model = weather
+    }
+    
+    var day: String {
+        return Self.dayFormatter.string(from: model.date)
+    }
+    
+    var month: String {
+        return Self.monthFormatter.string(from: model.date)
+    }
+    
+    var temperature: String {
+        return model.temprature.formatted()
+    }
+    
+    var title: String {
+        return model.label ?? "-"
+    }
 }
